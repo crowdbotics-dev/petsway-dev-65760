d50f2af040b6b0173cf3f912b885721b9dd2d2e7
Crowdbotics <engineering@crowdbotics.com>
2023-02-24 00:45:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/petsway_dev_65760/MainActivity.java
@@ -0,0 +1,15 @@
+package com.petsway_dev_65760;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "petsway_dev_65760";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "petsway-dev-65760",
+  url: "https://petsway-dev-65760.botics.co",
+  api: "https://petsway-dev-65760.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
