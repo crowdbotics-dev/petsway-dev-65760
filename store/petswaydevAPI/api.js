db24de3a7c6f2ada27aca4a6b630d15cc166c445
Aline Tavares <aline@crowdbotics.com>
2023-02-27 10:43:37 -0500

    Crowdbotics Studio: Update app screens

--- store/petswaydevAPI/api.js
+++ store/petswaydevAPI/api.js
@@ -1,55 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const petswaydevAPI = axios.create({
   baseURL: "https://petsway-dev-65760-prod.herokuapp.com",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_v1_login_create(payload) {
-  return petswaydevAPI.post(`/api/v1/login/`, payload.data)
+  return petswaydevAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return petswaydevAPI.post(`/api/v1/signup/`, payload.data)
+  return petswaydevAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return petswaydevAPI.get(`/rest-auth/user/`)
+  return petswaydevAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return petswaydevAPI.put(`/rest-auth/user/`, payload.data)
+  return petswaydevAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return petswaydevAPI.patch(`/rest-auth/user/`, payload.data)
+  return petswaydevAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 function api_docs_schema_retrieve(payload) {
   return petswaydevAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function rest_auth_login_create(payload) {
-  return petswaydevAPI.post(`/rest-auth/login/`, payload.data)
+  return petswaydevAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return petswaydevAPI.get(`/rest-auth/logout/`)
+  return petswaydevAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return petswaydevAPI.post(`/rest-auth/logout/`)
+  return petswaydevAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_registration_create(payload) {
-  return petswaydevAPI.post(`/rest-auth/registration/`, payload.data)
+  return petswaydevAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return petswaydevAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return petswaydevAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_change_create(payload) {
-  return petswaydevAPI.post(`/rest-auth/password/change/`, payload.data)
+  return petswaydevAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return petswaydevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return petswaydevAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return petswaydevAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return petswaydevAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 export const apiService = {
   api_v1_login_create,
   api_v1_signup_create,
@@ -65,4 +82,4 @@ export const apiService = {
   rest_auth_password_change_create,
   rest_auth_password_reset_confirm_create,
   rest_auth_registration_verify_email_create
-}
+};
