8f2408fb3344cdbe8c31392ed34fac8f8024fe8f
crowdbotics-dev <engineering@crowdbotics.com>
2023-03-01 06:25:02 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -19,6 +19,6 @@ class User(AbstractUser):
     everything that relates with an `User` is represented by this model.
     """
     name = models.CharField(null=True,blank=True,max_length=255,)
-    was_active_today = models.BooleanField(null=True,blank=True,)
+    new_field = models.BigIntegerField(null=True,blank=True,)
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
