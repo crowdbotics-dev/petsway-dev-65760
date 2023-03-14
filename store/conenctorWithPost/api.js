0781de20e7b396a2a982288f43f2f8d019eb4383
Crowdbotics <engineering@crowdbotics.com>
2023-03-14 17:14:17 +0000

    Update store.

---
+++ store/conenctorWithPost/api.js
@@ -0,0 +1,9 @@
+import axios from "axios"
+const conenctorWithPost = axios.create({
+  baseURL: "https://localhost.com/api/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function conenctorwithpost_post_user_create(payload) {
+  return conenctorWithPost.post(`/user`, payload.data)
+}
+export const apiService = { conenctorwithpost_post_user_create }
